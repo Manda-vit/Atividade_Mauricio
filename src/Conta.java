12630ee3987f51a4d1fa69a080a2529fa76693dc
giboxz <gilvanfsfilho@fac.pe.senac.br>
2022-10-05 12:12:42 -0300

    Correção erro conceitual

--- src/Conta.java
+++ src/Conta.java
@@ -1,13 +1,18 @@
-public class Conta extends Pessoa {
+public class Conta {
   private String numero;
   private String agencia;
   private double saldo;
+  private String cpf;
+  private String nome;
+  private String endereco;
 
-  public Conta(String cpf, String nome, String endereco, String numero, String agencia, double saldo) {
-    super(cpf, nome, endereco);
+  public Conta(String cpf, String nome, String endereco, String numero, String agencia, Double saldo) {
     this.numero = numero;
     this.agencia = agencia;
-   
+    this.cpf = cpf;
+    this.nome = nome;
+    this.endereco = endereco;
+
     if (saldo < 0) {
       System.out.println("O saldo não pode ser negativo, então ele foi setado como ZERO");
       this.saldo = 0;
@@ -19,28 +24,39 @@ public class Conta extends Pessoa {
   public String getNumero() {
     return numero;
   }
-
   public void setNumero(String numero) {
     this.numero = numero;
   }
-
   public String getAgencia() {
     return agencia;
   }
-
   public void setAgencia(String agencia) {
     this.agencia = agencia;
   }
-
   public double getSaldo() {
     return saldo;
   }
-
   public void setSaldo(double saldo) {
-    if (saldo >= 0) {
-      this.saldo = saldo;
-    } else {
-      System.out.println("valor não pode ser negativo.");
-    }
+    this.saldo = saldo;
   }
+  public String getCpf() {
+    return cpf;
+  }
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getEndereco() {
+    return endereco;
+  }
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  
 }
--- src/Login.java
+++ src/Login.java
@@ -1,17 +1,40 @@
-public class Login extends Pessoa {
+public class Login {
   private String senha;
+  private String cpf;
+  private String nome;
+  private String endereco;
 
-  public Login(String cpf, String nome, String endereco, String senha) {
-    super(cpf, nome, endereco);
+  public Login(String nome, String cpf, String endereco, String senha) {
     this.senha = senha;
+    this.cpf = cpf;
+    this.nome = nome;
+    this.endereco = endereco;
   }
 
   public String getSenha() {
     return senha;
   }
-
   public void setSenha(String senha) {
     this.senha = senha;
   }
+  public String getCpf() {
+    return cpf;
+  }
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getEndereco() {
+    return endereco;
+  }
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
 
 }
